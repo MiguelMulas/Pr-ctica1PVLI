c74d5c8bc68d50e80d49d74e59b5d37be83e61ac
Óscar Pardo Higuera <scar Pardo Higuera>
2016-11-13 14:02:57 +0100

    Tests TurnList superados

--- src/TurnList.js
+++ src/TurnList.js
@@ -15,11 +15,38 @@ TurnList.prototype.next = function () {
   // Haz que calcule el siguiente turno y devuelva el resultado
   // según la especificación. Recuerda que debe saltar los personajes
   // muertos.
+  var nextTurn = {number: 0, party: null, activeCharacterId: null};
+  this.turnNumber++;
+  var cont = 0;
+  var flag = true;
+  //var self = this;
+
+  while (flag && cont < this.list.length){
+    if (!this._charactersById[this.list[cont]]._isDead){
+      nextTurn.number = this.turnNumber;
+      this.activeCharacterId = this.list[cont];
+      nextTurn.activeCharacterId = this.list[cont];
+      //tambien vale igualarlo a this.activeCharacterId
+      nextTurn.party = this._charactersById[nextTurn.activeCharacterId].party;
+      flag = false;
+    }
+    cont++;
+  }
+  //while (this.list[acu]._isDead)
+
+  return nextTurn;
+  //if (!this.isDead)
 };
 
 TurnList.prototype._sortByInitiative = function () {
   // Utiliza la función Array.sort(). ¡No te implementes tu propia
   // función de ordenación!
+  var self = this;
+  var listaKeys = Object.keys (self._charactersById);
+  listaKeys.sort(function (a,b) {
+    return self._charactersById[b].initiative - self._charactersById[a].initiative;
+  });
+  return listaKeys;
 };
 
 module.exports = TurnList;
